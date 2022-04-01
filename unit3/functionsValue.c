d27629c67306513b353cd7fb82aaf035e9942d4b
germr <98335188+germr@users.noreply.github.com>
2022-03-31 23:29:40 -0600

    functions value

---
+++ unit3/functionsValue.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    bool isEven = isEvenFunct(2);
+    printf("result = %d\n", isEven);
+    int varA = 5;
+    int varB = 7;
+
+    ///varA = 5, varB = 7
+
+    printf("before function: varA= %d, varB= ")
+}
+
+printf("VarA=%d, VarB=%d",VarA,VarB)
+    swap (4,7);
+    //swap (7,4)
+    printf("VarA=%d, VarB=%d",VarA,VarB)
+    return 0;
+}
